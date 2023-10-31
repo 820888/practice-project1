d1e8c1b7030502de3cbc4d1e801e615dfe197d55
820888 <148947028+820888@users.noreply.github.com>
2023-10-31 17:15:15 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+package Javaprogram;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        for (int x = 2; x <= 4; x++) 
+            System.out.println("Value of x:" + x); 
+
+	}
+
+}
