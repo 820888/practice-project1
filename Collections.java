8dc72cf01d0427110aa7dc5ac3c1188e3bce690e
820888 <148947028+820888@users.noreply.github.com>
2023-10-31 17:19:28 +0530

    Add files via upload

---
+++ Collections.java
@@ -0,0 +1,16 @@
+package Javaprogram;
+import java.util.Vector;
+public class Collections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n");
+	      System.out.println("Vector");
+	      Vector<Integer> vec = new Vector<Integer>();
+	      vec.addElement(15); 
+	      vec.addElement(30); 
+	      System.out.println(vec);
+		
+	}
+		
+}
