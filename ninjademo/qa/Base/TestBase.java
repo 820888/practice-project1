08e8e9e7f99a9c944361d4f883d20886aef6534a
sameena shaik <148947028+820888@users.noreply.github.com>
2023-11-19 22:43:48 +0530

    Add files via upload

---
+++ ninjademo/qa/Base/TestBase.java
@@ -0,0 +1,62 @@
+package com.ninjademo.qa.Base;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.github.dockerjava.transport.DockerHttpClient.Request.Method;
+
+public class TestBase {
+	//Base class indicated the driver session and Application details
+	public static WebDriver driver;
+	
+	public static void openBrowser(String browser) {
+		
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://tutorialsninja.com/demo/");
+	}
+	@BeforeSuite // this method will be excuted before suite begins its execution
+	public void InitalizeExtentReport() {
+		ExtentSparkReporter sparkreporter = new ExtentSparkReporter("register_user_report.html");
+		extentreports = new ExtentReports();
+		extentreports.attachReporter(sparkreporter);
+		// on the report display more information about OS, browser, java etc
+		extentreports.setSystemInfo("OS", System.getProperty("os.name"));
+		extentreports.setSystemInfo("JAVA", System.getProperty("java.version"));
+
+	}
+	@BeforeTest
+	public void getnameMethod(ITestContext context) {
+		extentTest = extentreports.createTest(context.getName());
+	}
+	@AfterSuite
+	public void generateReports() throws IOException {
+		extentreports.flush();
+		Desktop.getDesktop().browse(new File("register_user_report.html").toURI());
+	}
+	@AfterMethod
+	public void generateTestStatus(Method m, ITestResult result) {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			System.out.println("Capture Screenshot");
+			extentTest.fail(result.getThrowable());
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+			extentTest.pass(m.getName() + " is passed");
+		}
+	}
+}
---
+++ ninjademo/qa/TestPages/TestRegisterPage.java
@@ -0,0 +1,44 @@
+package com.ninjademo.qa.TestPages;
+
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.ninjademo.qa.Base.TestBase;
+import com.ninjademo.qa.Utilities.Xls_DataProvider;
+import com.ninjademo.qa.pages.Homepage;
+import com.ninjademo.qa.pages.RegisterPage;
+
+public class TestRegisterPage extends TestBase {
+
+	Homepage hp;
+	RegisterPage rp;
+
+	@BeforeMethod
+	public void openApplication() {
+
+		openBrowser("Chrome");
+
+		hp = new Homepage(driver);
+		rp = new RegisterPage(driver);
+	}
+	
+	@Test(priority = '1', dataProvider = "testdata")
+	public void RegisterUser(String firstname1, String lastname1, String emailid1, String phone1, String password1,
+			String confirmpassword1) {
+
+		hp.clickOnMyAccount();
+		hp.clickOnRegister();
+		rp.RegisterUser(firstname1, lastname1, emailid1, phone1, password1, confirmpassword1);
+
+	}
+	@DataProvider(name = "testdata")
+	public Object[][] datasupplier() throws IOException {
+
+		Object[][] input = Xls_DataProvider.getTestData("Sheet1");
+		return input;
+	}
+}
